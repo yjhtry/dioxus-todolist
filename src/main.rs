439566a15aa673614febf450fd5a79c1aeca98ea
yjh <860622588@qq.com>
2024-04-03 14:18:56 +0800

    chore: clean

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,8 @@ use log::LevelFilter;
 enum Route {
     #[route("/")]
     Home {},
-    #[route("/blog/:id")]
-    Blog { id: i32 },
+    #[route("/todo")]
+    Todo {},
 }
 
 fn main() {
@@ -26,28 +26,17 @@ fn App() -> Element {
 }
 
 #[component]
-fn Blog(id: i32) -> Element {
+fn Todo() -> Element {
     rsx! {
-        Link { to: Route::Home {}, "Go to counter" }
-        "Blog post {id}"
+        "Todo list"
     }
 }
 
 #[component]
 fn Home() -> Element {
-    let mut count = use_signal(|| 0);
-
     rsx! {
-        Link {
-            to: Route::Blog {
-                id: count()
-            },
-            "Go to blog 1 222 "
-        }
         div {
-            h1 { class: "text-blue-500", "High-Five counter: {count}" }
-            button { onclick: move |_| count += 1, "Up high!" }
-            button { onclick: move |_| count -= 1, "Down low!" }
+            "Home page"
         }
     }
 }
